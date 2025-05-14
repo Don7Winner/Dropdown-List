89c5e93b24ddb23b328046ce67b53b188cb01832
Don7Winner <chidubemweb@gmail.com>
2025-05-14 10:26:15 +0100

    Push files to github

---
+++ css/index.js
@@ -0,0 +1,46 @@
+$(document).ready(() => {
+  // Hide dropdowns and arrow-up icons initially
+  $(".dropdownlist").hide();
+  $(".arrow-up").hide();
+
+  // Click handler for Features
+  $(".features").click(() => {
+    // Toggle the Features dropdown
+    $(".list-item-feature").slideToggle();
+
+    // Toggle arrows for Features
+    $(".features .arrow-down").toggle();
+    $(".features .arrow-up").toggle();
+
+    // Ensure Company dropdown is hidden and arrows are reset
+    $(".list-item-company").hide();
+    $(".company .arrow-down").show();
+    $(".company .arrow-up").hide();
+
+    // Adjust padding
+    // $(".nav-list").css("paddingRight", "0");
+  });
+
+  // Click handler for Company
+  $(".company").click(() => {
+    // Toggle the Company dropdown
+    $(".list-item-company").slideToggle();
+
+    // Toggle arrows for Company
+    $(".company .arrow-down").toggle();
+    $(".company .arrow-up").toggle();
+
+    // Ensure Features dropdown is hidden and arrows are reset
+    $(".list-item-feature").hide();
+    $(".features .arrow-down").show();
+    $(".features .arrow-up").hide();
+
+    // Adjust padding
+    // $(".nav-list").css("paddingRight", "0");
+  });
+
+  // Click handler for menu
+  $("#menu").click(() => {
+    $(".nav-list").css("display", "block");
+  });
+});
